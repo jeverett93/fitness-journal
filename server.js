cb6510b22bf23445c2f2208883fee3ac567e4c66
Josh Everett <josheverett93@gmail.com>
2020-06-02 21:56:09 -0500

    added initial code and deployed to heroku

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+
+
+
+const apiRoutes = require("./routes/api");
+const htmlRoutes = require("./routes/html");
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/api", apiRoutes);
+app.use(htmlRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-journal", {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
