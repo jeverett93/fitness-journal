c1c582ac4999f58abbbd4923b4d081ddf6101762
Josh Everett <josheverett93@gmail.com>
2020-06-03 13:59:34 -0500

    added skeleton of api routes

---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Workout = require("../models/index.js");
+const app = express();
+
+app.get("/api/workouts", (req, res) => {
+
+});
+
+app.put("/api/workouts", (req, res) => {
+
+});
+
+app.post("/api/workouts", (req, res) => {
+
+});
+
+app.get("/api/workouts/range", (req, res) => {
+
+});
+
+module.exports = app;
