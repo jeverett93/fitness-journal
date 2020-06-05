aa5ee1f1a9469a182a0865aa7fc614d389d1278e
Josh Everett <josheverett93@gmail.com>
2020-06-04 22:31:10 -0500

    update POST route

--- routes/api.js
+++ routes/api.js
@@ -13,11 +13,65 @@ app.get("/workouts", (req, res) => {
 });
 
 app.put("/workouts/:id", (req, res) => {
-
+//   Workout.update(
+//     {
+//       _id: mongojs.ObjectId(req.params.id)
+//     },
+//     {
+//       $set: {
+//         title: req.body.title,
+//         note: req.body.note,
+//         modified: Date.now()
+//       }
+//     },
+//     (error, data) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.send(data);
+//       }
+//     }
+//   );
+  Workout.findByIdAndUpdate(req.params.id, 
+    {exercises: [
+      {
+        type: req.body.type,
+        name: req.body.name,
+        duration: req.body.duration,
+        weight: req.body.weight,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        distance: req.body.distance
+      }
+    ]}, 
+    {new: true}, (err, data) => {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        res.send(data);
+      }
+    });
 });
 
 app.post("/workouts", (req, res) => {
-  Workout.create(req.body)
+  let newWorkout = {
+    day: req.body.day,
+    exercises: [
+      {
+        type: req.body.type,
+        name: req.body.name,
+        duration: req.body.duration,
+        weight: req.body.weight,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        distance: req.body.distance
+      }
+    ]
+    
+  };
+
+  Workout.create(newWorkout)
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
